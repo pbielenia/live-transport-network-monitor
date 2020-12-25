510a24244ba7e3fb2f76ab8ef0e9ceebdfac236e
pbielenia <piotrbielenia0@gmail.com>
2020-12-25 14:28:07 +0100

    Add Boost as a dependency

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
+#include <boost/asio.hpp>
+#include <boost/system/error_code.hpp>
 #include <iostream>
 
 int main()
 {
-	std::cout << "It works" << std::endl;
-
+    boost::system::error_code error_code;
+    if (error_code) {
+        std::cout << "Error: " << error_code.message() << "\n";
+        return -1;
+    } else {
+        std::cout << "OK\n";
+        return 0;
+    }
 }
-
