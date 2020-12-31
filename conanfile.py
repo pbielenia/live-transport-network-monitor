7f71b866745543729689f7237eb663c58770f090
pbielenia <piotrbielenia0@gmail.com>
2020-12-31 02:05:53 +0100

    Add libcurl as a dependency

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,8 @@ class ConanPackage(ConanFile):
 
     requires = [
         'boost/1.74.0',
-        'openssl/1.1.1h'
+        'openssl/1.1.1h',
+        'libcurl/7.74.0'
     ]
 
     default_options = (
