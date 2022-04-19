2816887a1854d3553e7f79e0538f1b1fdb6e984a
Piotr Bielenia <piotrbielenia0@gmail.com>
2022-04-19 21:13:07 +0200

    Add conan to the project

---
+++ conanfile.py
@@ -0,0 +1,13 @@
+from importlib.metadata import requires
+from conans import ConanFile
+
+
+class ConanPackage(ConanFile):
+    name = 'network-monitor'
+    version = "0.1.0"
+
+    generators = 'cmake_find_package'
+
+    requires = [
+
+    ]
