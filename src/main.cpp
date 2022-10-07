a843c734b994bfb28e9e468c52d66c5394dfe17c
Piotr Bielenia <piotrbielenia0@gmail.com>
2022-10-08 01:27:12 +0200

    [playground] Connect to echo WebSocket server (async)

--- src/main.cpp
+++ src/main.cpp
@@ -6,74 +6,141 @@
 #include <iostream>
 #include <thread>
 
+using tcp = boost::asio::ip::tcp;
+using WebsocketStream = boost::beast::websocket::stream<boost::beast::tcp_stream>;
+
 void log(boost::system::error_code error_code)
 {
     std::cerr << "* Error: " << error_code.message() << std::endl;
 }
 
-int main()
+void OnMessageReceived(const boost::system::error_code &error,
+                       boost::beast::flat_buffer &response)
 {
-    using tcp = boost::asio::ip::tcp;
-
-    boost::asio::io_context io_context{};
-
-    boost::system::error_code error_code{};
-
-    // 1. Reolve the address
-    const std::string server_address{"ltnm.learncppthroughprojects.com"};
-    const std::string server_port{"80"};
-    const std::string server_target{"/echo"};
-    tcp::resolver resolver{io_context};
-    const auto resolver_results{resolver.resolve(server_address, server_port, error_code)};
-    if (error_code)
+    if (error)
     {
-        log(error_code);
-        return -1;
-    }
-    std::cout << "* Resolved the address" << std::endl;
-
-    // 2. Create a TCP connection to the resolved host through a socket object
-    tcp::socket tcp_socket{io_context};
-    tcp_socket.connect(*resolver_results, error_code);
-    if (error_code)
-    {
-        log(error_code);
-        return -2;
-    }
-    std::cout << "* Connected" << std::endl;
-
-    // 3. Use the socket object to construct a websocket::stream class
-    boost::beast::websocket::stream<boost::beast::tcp_stream> websocket_stream{std::move(tcp_socket)};
-
-    // 4. Use the websocket::stream instance to perform a WebSocket handshake
-    const std::string server_host{server_address + ':' + server_port};
-    websocket_stream.handshake(server_host, server_target, error_code);
-    if (error_code)
-    {
-        log(error_code);
-        return -3;
-    }
-    std::cout << "* Handshake succeded" << std::endl;
-
-    // 5. Send a message to the connected WebSocket server and receive an echo message
-    //    back using the websocket::stream::read and write APIs
-    const std::string message_content{"Dzien dobry"};
-    boost::asio::const_buffer message{message_content.c_str(), message_content.size()};
-    websocket_stream.write(message, error_code);
-    if (error_code)
-    {
-        log(error_code);
-        return -4;
-    }
-    std::cout << "* An empty message has been sent" << std::endl;
-
-    boost::beast::flat_buffer response{};
-    websocket_stream.read(response, error_code);
-    if (error_code)
-    {
-        log(error_code);
-        return -5;
+        log(error);
+        return;
     }
     std::cout << "* Received the message:\n"
               << boost::beast::make_printable(response.data()) << std::endl;
+}
+
+void OnMessageSent(const boost::system::error_code &error,
+                   WebsocketStream &websocket_stream,
+                   boost::beast::flat_buffer &response)
+{
+    if (error)
+    {
+        log(error);
+        return;
+    }
+    std::cout << "* An empty message has been sent" << std::endl;
+
+    websocket_stream.async_read(response, [&response](const boost::system::error_code &error, const size_t)
+                                { OnMessageReceived(error, response); });
+}
+
+void OnHandshakeCompleted(const boost::system::error_code &error,
+                          WebsocketStream &websocket_stream,
+                          const std::string &message_content,
+                          boost::beast::flat_buffer &response)
+{
+    if (error)
+    {
+        log(error);
+        return;
+    }
+    std::cout << "* Handshake succeded" << std::endl;
+
+    boost::asio::const_buffer message{message_content.c_str(), message_content.size()};
+    websocket_stream.async_write(message, [&websocket_stream, &response](const boost::system::error_code &error, const size_t)
+                                 { OnMessageSent(error, websocket_stream, response); });
+}
+
+void OnTcpSocketConnected(const boost::system::error_code &error,
+                          WebsocketStream &websocket_stream,
+                          const std::string &server_address,
+                          const std::string &server_port,
+                          const std::string &connection_target,
+                          const std::string &message_content,
+                          boost::beast::flat_buffer &response)
+{
+    if (error)
+    {
+        log(error);
+        return;
+    }
+    std::cout << "* Connected" << std::endl;
+
+    const std::string server_host{server_address + ':' + server_port};
+    websocket_stream.async_handshake(server_host, connection_target,
+                                     [&websocket_stream, &message_content, &response](
+                                         const boost::system::error_code &error)
+                                     {
+                                         OnHandshakeCompleted(error, websocket_stream,
+                                                              message_content, response);
+                                     });
+}
+
+void OnServerAddressResolved(const boost::system::error_code &error,
+                             tcp::resolver::results_type results,
+                             WebsocketStream &websocket_stream,
+                             const std::string &server_address,
+                             const std::string &server_port,
+                             const std::string &connection_target,
+                             const std::string &message_content,
+                             boost::beast::flat_buffer &response)
+{
+    if (error)
+    {
+        log(error);
+        return;
+    }
+    std::cout << "* Resolved the address" << std::endl;
+
+    websocket_stream.next_layer().async_connect(*results,
+                                                [&websocket_stream, &server_address,
+                                                 &server_port, &connection_target,
+                                                 &message_content, &response](
+                                                    const boost::system::error_code &error)
+                                                {
+                                                    OnTcpSocketConnected(error,
+                                                                         websocket_stream,
+                                                                         server_address,
+                                                                         server_port,
+                                                                         connection_target,
+                                                                         message_content,
+                                                                         response);
+                                                });
+}
+
+int main()
+{
+
+    boost::asio::io_context io_context{};
+    boost::system::error_code error_code{};
+
+    const std::string server_address{"ltnm.learncppthroughprojects.com"};
+    const std::string server_port{"80"};
+    const std::string connection_target{"/echo"};
+    const std::string message_content{"Dobry wieczor"};
+    boost::beast::flat_buffer response{};
+
+    tcp::resolver resolver{io_context};
+    tcp::socket tcp_socket{io_context};
+    boost::beast::websocket::stream<boost::beast::tcp_stream> websocket_stream{std::move(tcp_socket)};
+
+    resolver.async_resolve(server_address, server_port,
+                           [&websocket_stream, &server_address, &server_port,
+                            &connection_target, &message_content, &response](
+                               const boost::system::error_code &error,
+                               tcp::resolver::results_type results)
+                           {
+                               OnServerAddressResolved(error, results, websocket_stream,
+                                                       server_address, server_port,
+                                                       connection_target, message_content,
+                                                       response);
+                           });
+    io_context.run();
 }
