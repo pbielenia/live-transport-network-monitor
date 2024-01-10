6076382858404f24a56f7a7b64edc506be908f79
Piotr Bielenia <piotrbielenia0@gmail.com>
2024-01-10 23:29:47 +0100

    FUNCT: WIP: add some helper functions to make STOMP frames easier

--- inc/network-monitor/stomp-frame-builder.hpp
+++ inc/network-monitor/stomp-frame-builder.hpp
@@ -11,7 +11,7 @@ struct BuildParameters {
 
     StompCommand command;
     // TODO: StompFrame::Headers may be used possibly
-    std::unordered_map<StompHeader, std::string> headers;
+    StompFrame::Headers headers;
     std::string body;
 };
 
@@ -21,7 +21,29 @@ struct BuildParameters {
 // TODO: rename to `BuildFrame` or `MakeFrame`
 StompFrame Build(StompError& error_code, const BuildParameters& parameters);
 
-// StompFrame MakeConnectedFrame()
+// Server frames.
+StompFrame MakeConnectedFrame(
+    const std::string& version,
+    const std::string& session,
+    const std::string& server,
+    const std::string& heart_beat
+);
+StompFrame MakeErrorFrame(
+    const std::string& message,
+    const std::string& body
+);
+StompFrame MakeReceiptFrame(
+    const std::string& receipt_id
+);
+StompFrame MakeMessageFrame(
+    const std::string& destination,
+    const std::string& message_id,
+    const std::string& subscription,
+    const std::string& ack,
+    const std::string& body,
+    const std::string& content_length,
+    const std::string& content_type
+);
 
 }  // namespace stomp_frame
 }  // namespace NetworkMonitor
--- src/stomp-frame-builder.cpp
+++ src/stomp-frame-builder.cpp
@@ -2,15 +2,27 @@
 
 using namespace NetworkMonitor;
 
+namespace {
+    void EmplaceIfValueNotEmpty(StompFrame::Headers& headers, StompHeader header, const std::string& value)
+    {
+        if (!value.empty()) {
+            headers.emplace(header, value);
+        }
+    }
+}
+
 std::string ParseCommand(StompCommand command) {
     return ToString(command) + '\n';
 }
 
-std::string ParseHeaders(const std::unordered_map<StompHeader, std::string> headers) {
+std::string ParseHeaders(const StompFrame::Headers& headers) {
     std::string parsed{};
 
     for (const auto [header, value] : headers) {
-        parsed.append(ToString(header) + ':' + value + '\n');
+        parsed.append(ToString(header));
+        parsed.append(":");
+        parsed.append(value);
+        parsed.append("\n");
     }
     parsed.push_back('\n');
     return parsed;
@@ -30,4 +42,54 @@ StompFrame stomp_frame::Build(StompError& error_code, const BuildParameters& par
     plain_content.push_back('\0');
     
     return StompFrame{error_code, std::move(plain_content)};
+}
+
+StompFrame stomp_frame::MakeConnectedFrame(
+    const std::string& version,
+    const std::string& session,
+    const std::string& server,
+    const std::string& heart_beat)
+{
+    BuildParameters parameters(StompCommand::Connected);
+    parameters.headers.emplace(StompHeader::Version, version);
+    EmplaceIfValueNotEmpty(parameters.headers, StompHeader::Session, session);
+    EmplaceIfValueNotEmpty(parameters.headers, StompHeader::Server, server);
+    EmplaceIfValueNotEmpty(parameters.headers, StompHeader::HeartBeat, heart_beat);
+
+    StompError error;
+    return Build(error, parameters);
+}
+
+StompFrame stomp_frame::MakeErrorFrame(
+    const std::string& message,
+    const std::string& body)
+{
+    BuildParameters parameters(StompCommand::Error);
+    EmplaceIfValueNotEmpty(parameters.headers, StompHeader::Message, message);
+    if (!body.empty()) {
+        parameters.body = body;
+    }
+
+    StompError error;
+    return Build(error, parameters);
+}
+
+StompFrame MakeReceiptFrame(
+    const std::string& receipt_id)
+{
+    // TODO
+    return {};
+}
+
+StompFrame MakeMessageFrame(
+    const std::string& destination,
+    const std::string& message_id,
+    const std::string& subscription,
+    const std::string& ack,
+    const std::string& body,
+    const std::string& content_length,
+    const std::string& content_type)
+{
+    // TODO
+    return {};
 }
