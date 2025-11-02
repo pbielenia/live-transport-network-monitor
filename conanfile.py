e0c5889de220d6a52cbaf9bfa94388e94217de5b
Piotr Bielenia <piotrbielenia0@gmail.com>
2025-11-02 14:45:29 +0100

    Add Python black formatter

--- conanfile.py
+++ conanfile.py
@@ -3,15 +3,16 @@ from conan.tools.cmake import CMakeToolchain, CMakeDeps, cmake_layout
 
 
 class ConanPackage(ConanFile):
-    name = 'network-monitor'
+    name = "network-monitor"
     version = "0.1.0"
     settings = "os", "compiler", "build_type", "arch"
-    requires = ("boost/1.80.0",
-                "libcurl/7.85.0",
-                "nlohmann_json/3.11.2",
-                "openssl/3.6.0",
-                "zlib/1.2.13",
-                )
+    requires = (
+        "boost/1.80.0",
+        "libcurl/7.85.0",
+        "nlohmann_json/3.11.2",
+        "openssl/3.6.0",
+        "zlib/1.2.13",
+    )
     default_options = {"boost/*:shared": False}
 
     def generate(self):
