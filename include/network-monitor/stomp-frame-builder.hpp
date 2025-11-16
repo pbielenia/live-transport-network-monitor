7975fe646ecc04546470ab3484088f1c578c6c1b
Piotr Bielenia <piotrbielenia0@gmail.com>
2025-11-16 17:41:42 +0100

    [stomp-frame-builder] Remove unused builder free functions

--- include/network-monitor/stomp-frame-builder.hpp
+++ include/network-monitor/stomp-frame-builder.hpp
@@ -21,22 +21,4 @@ class StompFrameBuilder {
   std::string body_;
 };
 
-StompFrame MakeFrameConnected(std::string_view version,
-                              std::string_view session,
-                              std::string_view server,
-                              std::string_view heart_beat);
-StompFrame MakeFrameError(std::string_view message, std::string_view body = {});
-StompFrame MakeFrameReceipt(std::string_view receipt_id);
-StompFrame MakeFrameMessage(std::string_view destination,
-                            std::string_view message_id,
-                            std::string_view subscription,
-                            std::string_view ack,
-                            std::string_view body,
-                            std::string_view content_length,
-                            std::string_view content_type);
-StompFrame MakeFrameSubscribe(std::string_view destination,
-                              std::string_view id,
-                              std::string_view ack,
-                              std::string_view receipt);
-
 }  // namespace network_monitor
